0b9662c3f1bff0a5f1f8ee1990f0bb112f6aceba
fernandoliveira8968 <32103618+fernandoliveira8968@users.noreply.github.com>
2017-11-24 16:16:53 +0000

    Add files via upload

---
+++ HojeSextaFeira.java
@@ -0,0 +1,10 @@
+package hojesextafeira;
+public class HojeSextaFeira { 
+    public static void main(String[] args) {
+        final  String mensagem= "Hoje é sexta feira";
+    for(int I=1; I<=10;I++)    
+    System.out.println(mensagem);
+            
+    }
+    
+}
