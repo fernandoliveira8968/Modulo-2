6a54d3879d36f9d3ec8e4b0470eff63822753807
fernandoliveira8968 <32103618+fernandoliveira8968@users.noreply.github.com>
2017-12-14 15:57:36 +0000

    Add files via upload

---
+++ Fatorial.java
@@ -0,0 +1,21 @@
+
+package fatorial;
+
+import java.util.Scanner;
+
+
+public class Fatorial {
+
+    
+    public static void main(String[] args) {
+    int n, fat=1;
+    Scanner lerS = new Scanner (System.in);
+    System.out.print("Digite um numero inferior a 32");
+    n = lerS.nextInt();
+    for (int m=n; m>=1; m--)
+        fat*=m;
+    System.out.printf(" %d! = %d ", n, fat);
+             
+    }
+ 
+}
